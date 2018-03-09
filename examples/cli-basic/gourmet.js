a29cb447bb5cdfd62d5871395735f7a0be68219d
Max Jung <max@gourmetjs.com>
2018-03-09 17:33:51 +0900

    Add comment to examples/cli-basic

--- examples/cli-basic/gourmet.js
+++ examples/cli-basic/gourmet.js
@@ -1,6 +1,11 @@
 "use strict";
 
 module.exports = {
+  // Three user plugins will be loaded:
+  //  1. "@gourmet/example-cli-test-plugin" - auto loaded
+  //  2. "@gourmet/example-cli-test-plugin" - manually by `plugins: [...]`
+  //     (located here because of `before: ".../my-plugin"` in `meta.schema`)
+  //  3. "@gourmet/example-cli-basic/my-plugin" - manually by `plugins: [...]`
   plugins: [{
     name: "@gourmet/example-cli-basic/my-plugin",
     plugin: require.resolve("./lib/MyPlugin")
