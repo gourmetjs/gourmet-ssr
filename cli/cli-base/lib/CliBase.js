e5a3352215c2607d298b0e93a8dbd823b1a90d2a
Max Jung <max@gourmetjs.com>
2018-03-16 00:53:33 +0900

    Add 'vars' to @gourmet/gourmet-cli

--- cli/cli-base/lib/CliBase.js
+++ cli/cli-base/lib/CliBase.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const npath = require("path");
-const fs = require("fs");
 const minimist = require("minimist");
 const omit = require("lodash.omit");
 const runAsMain = require("promise-box/lib/runAsMain");
@@ -10,11 +9,6 @@ const error = require("@gourmet/error");
 const merge = require("@gourmet/merge");
 const PluginManager = require("./PluginManager");
 const PluginBuiltinHelp = require("./PluginBuiltinHelp");
-
-const CONFIG_FILE_NOT_FOUND = {
-  message: "Config file '${baseName}.js' or '${baseName}.json' not found in directory '${workDir}'",
-  code: "CONFIG_FILE_NOT_FOUND"
-};
 
 const COMMAND_OPTION_REQUIRED = {
   message: "`Option '--${name}' is required`",
@@ -35,35 +29,23 @@ class CliBase {
   constructor(cliConfig={}) {
     this._cliConfig = merge({
       workDirArgName: "dir",
-      configBaseName: "cli",
       builtinPlugins: [{
         name: PluginBuiltinHelp.meta.name,
         plugin: PluginBuiltinHelp
       }],
-      isPackageRequired: false,
-      isConfigRequired: true,
-      autoLoadPlugins: "prepend",    // false | true | "prepend" | "append"
       defaultCommand: "help",
-      configPropNames: {
-        plugins: "plugins",
-        autoLoadPlugins: "autoLoadPlugins"
-      },
       PluginManager
     }, cliConfig);
-  }
-
-  // Override and return `true` if the dependency is an auto-loadable plugin
-  isAutoLoadablePlugin(moduleName) {   // eslint-disable-line no-unused-vars
-    return false;
   }
 
   runCommand(argv) {
     runAsMain(
       wrap(() => {
-        argv = minimist(argv);
-        this._init(argv[this._cliConfig.workDirArgName]);
-        return this._parseCommandArgs(argv);
-      }).then(options => {
+        return this.init(argv);
+      }).then(() => {
+        return this._applyCommandInfo();
+      }).then(() => {
+        const options = this.options;
         return Promise.resolve().then(() => {
           return this.plugins.runAsync("before:command:" + options.$command, options, this);
         }).then(() => {
@@ -86,93 +68,51 @@ class CliBase {
     );
   }
 
-  _init(workDir="") {
-    const clicfg = this._cliConfig;
-    const PluginManager = clicfg.PluginManager;
+  init(argv) {
+    const PluginManager = this._cliConfig.PluginManager;
 
-    this.workDir = npath.resolve(process.cwd(), workDir);
-    this.package = this._loadModule(npath.join(this.workDir, "package.json"), clicfg.isPackageRequired);
+    argv = minimist(argv);
 
-    this._config = this._loadConfig();
-
+    this.options = this._parseCommandArgs(argv);
+    this.workDir = npath.resolve(process.cwd(), this.options.$workDir);
     this.plugins = new PluginManager(this);
 
-    this._loadBuiltinPlugins();
-    this._loadUserPlugins();
+    return wrap(() => {
+      return this.loadBuiltinPlugins();
+    }).then(() => {
+      return this.loadConfig();
+    }).then(() => {
+      return this.loadUserPlugins();
+    });
   }
 
-  _loadModule(path, required) {
-    try {
-      return require(path);
-    } catch (err) {
-      if (required || err.code !== "MODULE_NOT_FOUND")
-        throw err;
-      return null;
-    }
-  }
-
-  _loadConfig() {
-    const baseName = this._cliConfig.configBaseName;
-    const required = this._cliConfig.isConfigRequired;
-    const exts = [".js", ".json"];
-
-    for (let idx = 0; idx < exts.length; idx++) {
-      const ext = exts[idx];
-      const path = npath.join(this.workDir, baseName + ext);
-      if (fs.existsSync(path))
-        return require(path);
-    }
-
-    if (required)
-      throw error(CONFIG_FILE_NOT_FOUND, {baseName, workDir: this.workDir});
-  }
-
-  _loadBuiltinPlugins() {
+  loadBuiltinPlugins() {
     const plugins = this._cliConfig.builtinPlugins;
     this.plugins.load(plugins, __dirname);
   }
 
-  _loadUserPlugins() {
-    const auto = this._cliConfig.autoLoadPlugins;
-    const names = this._cliConfig.configPropNames;
-
-    let plugins = this._config[names.plugins] || [];
-
-    if (auto === true) {
-      if (!plugins.length)
-        plugins = this._scanPlugins(auto);
-    } else if (auto === "prepend") {
-      plugins = this._scanPlugins(auto).concat(plugins);
-    } else if (auto === "append") {
-      plugins = plugins.concat(this._scanPlugins(auto));
-    }
-
-    this.plugins.load(plugins, this.workDir);
+  loadConfig() {
   }
 
-  _scanPlugins(auto) {
-    if (auto && this.package) {
-      const deps = Object.assign({}, this.package.dependencies, this.package.devDependencies);
-      return Object.keys(deps).filter(dep => this.isAutoLoadablePlugin(dep));
-    }
-    return [];
+  loadUserPlugins() {    
   }
 
   _parseCommandArgs(argv) {
     const options = omit(argv, ["_", this._cliConfig.workDirArgName]);
-    let command = argv._.join(" ");
+    const workDir = argv[this._cliConfig.workDirArgName] || "";
+    const command = argv._.join(" ") || this._cliConfig.defaultCommand;
 
-    if (!command)
-      command = this._cliConfig.defaultCommand;
-
-    // Make `options.$command` invisible to regular enumeration.
+    // Make `options.$command` & `options.$workDir` invisible to regular enumeration.
     Object.defineProperty(options, "$command", {value: command});
-
-    const info = this._findCommandInfo(options.$command);
-
-    this._verifyCommandOptions(options, info);
+    Object.defineProperty(options, "$workDir", {value: workDir});
 
     return options;
+  }
+
+  _applyCommandInfo() {
+    const options = this.options;
+    const info = this._findCommandInfo(options.$command);
+    this._verifyCommandOptions(options, info);
   }
 
   _findCommandInfo(command) {
--- cli/gourmet-cli/lib/GourmetCli.js
+++ cli/gourmet-cli/lib/GourmetCli.js
@@ -1,17 +1,97 @@
 "use strict";
 
+const npath = require("path");
+const fs = require("fs");
 const resolve = require("resolve");
+const isPromise = require("promise-box/lib/isPromise");
+const error = require("@gourmet/error");
 const CliBase = require("@gourmet/cli-base");
+const Variables = require("@gourmet/variables");
+const Self = require("@gourmet/variables/lib/sources/Self");
+const Env = require("@gourmet/variables/lib/sources/Env");
+const Opt = require("@gourmet/variables/lib/sources/Opt");
+const File = require("@gourmet/variables/lib/sources/File");
+
+const CONFIG_FILE_NOT_FOUND = {
+  message: "Config file 'gourmet.js' or 'gourmet.json' not found in directory '${workDir}'",
+  code: "CONFIG_FILE_NOT_FOUND"
+};
 
 class GourmetCli extends CliBase {
-  constructor() {
-    super({
-      configBaseName: "gourmet",
-      builtinPlugins: []
+  loadConfig() {
+    const exts = [".js", ".json"];
+
+    this.package = this._loadModuleSafe(npath.join(this.workDir, "package.json"));
+
+    for (let idx = 0; idx < exts.length; idx++) {
+      const ext = exts[idx];
+      const path = npath.join(this.workDir, "gourmet" + ext);
+
+      if (fs.existsSync(path)) {
+        let config = require(path);
+
+        if (typeof config === "function")
+          config = config(this);
+
+        if (!isPromise(config))
+          config = Promise.resolve(config);
+
+        return config.then(config => {
+          this._initVars(config);
+        });
+      }
+    }
+
+    throw error(CONFIG_FILE_NOT_FOUND, {workDir: this.workDir});
+  }
+
+  loadUserPlugins() {
+    return Promise.all([
+      this.vars.get("autoLoadPlugins"),
+      this.vars.get("plugins")
+    ]).then(([
+      auto="prepend",   // false | true | "prepend" | "append"
+      plugins=[]
+    ]) => {
+      if (auto === true) {
+        if (!plugins.length)
+          plugins = this._scanPlugins(auto);
+      } else if (auto === "prepend") {
+        plugins = this._scanPlugins(auto).concat(plugins);
+      } else if (auto === "append") {
+        plugins = plugins.concat(this._scanPlugins(auto));
+      }
+      this.plugins.load(plugins, this.workDir);
     });
   }
 
-  isAutoLoadablePlugin(moduleName) {
+  _initVars(config) {
+    const vars = this.vars = new Variables(config);
+    vars.addSource("self", new Self(vars));
+    vars.addSource("env", new Env());
+    vars.addSource("opt", new Opt(this.options));
+    vars.addSource("file", new File(vars, this.workDir, this));
+  }
+
+  _loadModuleSafe(path) {
+    try {
+      return require(path);
+    } catch (err) {
+      if (err.code !== "MODULE_NOT_FOUND")
+        throw err;
+      return null;
+    }
+  }
+  
+  _scanPlugins(auto) {
+    if (auto && this.package) {
+      const deps = Object.assign({}, this.package.dependencies, this.package.devDependencies);
+      return Object.keys(deps).filter(dep => this._isAutoLoadablePlugin(dep));
+    }
+    return [];
+  }
+
+  _isAutoLoadablePlugin(moduleName) {
     const path = resolve.sync(moduleName + "/package.json", {basedir: this.workDir});
     const pkg = require(path);
     const keywords = pkg.keywords;
