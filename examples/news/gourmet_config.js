cb0189d193b4c68a571f8798ceafadc6d2c80226
Max Jung <max@gourmetjs.com>
2018-05-07 18:43:13 +0900

    [examples/news] Change fontawesome's CDN url from 'http://...' to '//...'

--- examples/news/gourmet_config.js
+++ examples/news/gourmet_config.js
@@ -8,7 +8,7 @@ module.exports = {
     html: {
       headTop: [
         '<link href="//stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">',
-        '<link href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" rel="stylesheet" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">'
+        '<link href="//use.fontawesome.com/releases/v5.0.12/css/all.css" rel="stylesheet" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">'
       ]
     }
   }
