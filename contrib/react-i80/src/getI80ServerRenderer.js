496b7c418f2558921266a9e92cdca0f06d93780d
Max Jung <max@gourmetjs.com>
2018-08-30 14:47:52 -0700

    Revert "Revert "Make react-i80 silent when i80() hasn't been called for non-react-i80 projects""
    This reverts commit d72817ff592234cd8ccbcbcee7f77b3c0934ddda.

--- contrib/react-i80/src/getI80ServerRenderer.js
+++ contrib/react-i80/src/getI80ServerRenderer.js
@@ -11,27 +11,34 @@ module.exports = function(Base) {
   return class I80ServerRenderer extends Base {
     createContext(...args) {
       const gmctx = super.createContext(...args);
-      gmctx.routerData = {initialProps: {}};
-      gmctx.redirect = function(location, statusCode=302, content) {
-        location = encodeUrl(location);
-        content = content || `<p>[${statusCode}] Redirecting to ${escapeHtml(location)}...</p>`;
-        gmctx.result = {
-          statusCode,
-          headers: {location},
-          content
+      const router = Router.get(true);
+      if (router) {
+        gmctx.routerData = {initialProps: {}};
+        gmctx.redirect = function(location, statusCode=302, content) {
+          location = encodeUrl(location);
+          content = content || `<p>[${statusCode}] Redirecting to ${escapeHtml(location)}...</p>`;
+          gmctx.result = {
+            statusCode,
+            headers: {location},
+            content
+          };
+          return true;
         };
-        return true;
-      };
+      }
       return gmctx;
     }
 
     invokeUserRenderer(gmctx) {
-      const router = Router.get();
-      const href = router.getTargetHref(gmctx);
-      return router.setActiveRoute(gmctx, href).then(cont => {
-        if (cont)
-          return super.invokeUserRenderer(gmctx);
-      });
+      const router = Router.get(true);
+      if (router) {
+        const href = router.getTargetHref(gmctx);
+        return router.setActiveRoute(gmctx, href).then(cont => {
+          if (cont)
+            return super.invokeUserRenderer(gmctx);
+        });
+      } else {
+        return super.invokeUserRenderer(gmctx);
+      }
     }
   };
 };
