2dfbd8b4ca195d7184a33dd56cac413d7a444f70
Max Jung <max@gourmetjs.com>
2018-09-14 12:08:44 -0700

    Update comments

--- runtime/watch-middleware/lib/embedClient.js
+++ runtime/watch-middleware/lib/embedClient.js
@@ -6,9 +6,6 @@ const merge = require("@gourmet/merge");
 const omit = require("@gourmet/omit");
 const escapeScript = require("@gourmet/escape-script");
 
-// Note about browser WebSocket behavior:
-// It appears that "close" comes even without "open" for the initial failure.
-// This behavior is observed in Chrome, Edge & Firefox.
 module.exports = function embedClient(gourmet, options) {
   const client = fs.readFileSync(npath.join(__dirname, "client.js.txt"), "utf8");
   const opts = Object.assign({
