e375f33b3b1dbd9455b5b7758b0b3daab4242220
Max Jung <max@gourmetjs.com>
2018-11-07 10:32:37 -0800

    [TEMP] Experiment Babel7 features using react-hello

---
+++ tests/react-hello/src/.babelrc.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports = {
+  plugins: ["@babel/plugin-proposal-class-properties"]
+};
--- tests/react-hello/src/HelloApp.jsx
+++ tests/react-hello/src/HelloApp.jsx
@@ -14,6 +14,8 @@ export default class HelloApp extends PureComponent {
     );
   }
 
+  message = "Hello";
+  
   render() {
     return (
       <div className="container">
