f1c282cd5775fe8612d9a2e7f6f0ffb708065506
Max Jung <max@gourmetjs.com>
2019-02-12 18:08:30 -0800

    Remove `@gourmet/preset-react-emotion`
    Use `@gourmet/preset-react` and `@gourmet/group-react-emotion` instead.

--- groups/react-emotion/lib/GourmetGroupReactEmotion.js
+++ groups/react-emotion/lib/GourmetGroupReactEmotion.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// 8/17/2018 Added `prop-types` to prevent the following warning from yarn:
+//   "@gourmet/group-react-emotion > react-emotion > create-emotion-styled@9.2.6" has unmet peer dependency "prop-types@15.x".
 class GroupReactEmotion {
   onUserConfig(context) {
     const moduleDir = context.builder.moduleDir(__dirname);
